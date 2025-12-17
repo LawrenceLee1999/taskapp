4d6f654f9ed4a40f9affaacb843b50860763c859
LawrenceLee1999 <lawrencelee1999@hotmail.co.uk>
2025-12-17 11:28:33 +0000

    Initial Spring Boot project setup

---
+++ src/test/java/com/lawrence/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrence.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
